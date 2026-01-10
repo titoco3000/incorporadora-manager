376bc15e3634115c5d236f6dcbec4dab1e36c72a
Tito Guidotti <tito.guidotti@gmail.com>
2026-01-10 15:43:04 +0100

    Remove placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,4 @@
 <script lang="ts">
-  export let data;
 </script>
 
-<h1>Transaction Types</h1>
-
-<form method="POST">
-  <input name="name" placeholder="Name (e.g. Groceries)" required />
-  <label>
-    <input type="checkbox" name="isExpense" checked /> Is Expense?
-  </label>
-  <button type="submit">Add Type</button>
-</form>
-
-<hr />
-
-<ul>
-  {#each data.types as type}
-    <li>
-      {type.name} — <strong>{type.isExpense ? 'Expense' : 'Income'}</strong>
-    </li>
-  {/each}
-</ul>
+<h1>Aqui seria o dashboard!</h1>
