f66856ee74ccfe3ff5e96f12262c6097ed815d0e
titoco3000 <tito.guidotti@gmail.com>
2026-01-21 10:11:49 +0100

    Adiciona layout de dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,92 @@
 <script lang="ts">
 </script>
+<main>
 
-<h1>Aqui seria o dashboard!</h1>
+	<header>
+		<div class="title-block">
+			<h2>Incorporadora</h2>
+			<h1>Costa Leste</h1>
+		</div>
+		<div class="header-controls">
+			<label>
+				<span>De</span>
+				<input type="date" name="" id="">
+			</label>
+			<label>
+				<span>Até</span>
+				<input type="date" name="" id="">
+			</label>
+		</div>
+	</header>
+	<div class="panels">
+		<div class="panel-container top-row">
+			<div class="panel simple-data-panel"></div>
+			<div class="panel simple-data-panel"></div>
+			<div class="panel simple-data-panel"></div>
+			<div class="panel simple-data-panel"></div>
+		</div>
+		<div class="panel-container column wide-column">
+			<div class="panel gastos-por-area">
+				<h3>Gastos por area</h3>
+			</div>
+			<div class="panel volume-imoveis">
+				<h3>
+					Entradas e saidas por Imóvel
+				</h3>
+			</div>
+		</div>
+		<div class="panel-container column thin-column">
+			<div class="panel next-contract-end"></div>
+			<div class="panel main-suppliers"></div>
+		</div>
+	</div>
+</main>
+<style>
+	main{
+		padding: 0 50px;
+	}
+	header{
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		margin: 30px 0;
+	}
+	h2{
+		color: var(--text-color-1);
+		font-size: 1em;
+		font-weight: 100;
+	}
+	.panels{
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.panel-container{
+		display: flex;
+	}
+	.panel{
+		background-color: var(--bg-color-2);
+		min-height: 100px;
+		min-width: 100px;
+		margin: 10px;
+		border: 1px solid var(--border-color-1);
+		border-radius: 10px;
+		flex-grow: 1;
+	}
+	.top-row{
+		width: 100%;
+	}
+	.column{
+		display: flex;
+		flex-direction: column;
+	}
+	.wide-column{
+		min-width: 60%;
+		flex-grow: 3/5;
+	}
+	.gastos-por-area{
+		min-width: 400px;
+	}
+	.thin-column{
+		min-width: 40%;
+	}
+</style>
